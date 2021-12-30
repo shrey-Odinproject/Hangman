6604a2f208fdf3e1f29e975520c0e6392c710759
shrey-Odinproject <sonishreyf1@gmail.com>
2021-12-30 13:55:21 +0530

    added a function to display hangman art and function to ask for guess and a barebones play function

--- hangman.rb
+++ hangman.rb
@@ -1,17 +1,107 @@
-
-def make_sec_word
-  file=File.open('5desk.txt', "r")
+def make_sec_word # creates a word to guess
+  file = File.open('5desk.txt', "r")
   lines = file.readlines
-  line=lines[rand(61408)]
+  line = lines[rand(61406)]
   file.close
-  if line.length > 4 && line.length<13
+  if line.length > 4 && line.length < 13
     line
   else
     make_sec_word
   end
 end
 
-20.times {puts make_sec_word}
+def display_hangman(tries) # takes in num of tries and displays appropriate figure
+  stages = [  # final state: head, torso, both arms, and both legs
+              """
+                 --------
+                 |      |
+                 |      O
+                 |     \\|/
+                 |      |
+                 |     / \\
+                 -
+              """,
+              # head, torso, both arms, and one leg
+              """
+                 --------
+                 |      |
+                 |      O
+                 |     \\|/
+                 |      |
+                 |     / 
+                 -
+              """,
+              # head, torso, and both arms
+              """
+                 --------
+                 |      |
+                 |      O
+                 |     \\|/
+                 |      |
+                 |      
+                 -
+              """,
+              # head, torso, and one arm
+              """
+                 --------
+                 |      |
+                 |      O
+                 |     \\|
+                 |      |
+                 |     
+                 -
+              """,
+              # head and torso
+              """
+                 --------
+                 |      |
+                 |      O
+                 |      |
+                 |      |
+                 |     
+                 -
+              """,
+              # head
+              """
+                 --------
+                 |      |
+                 |      O
+                 |    
+                 |      
+                 |     
+                 -
+              """,
+              # initial empty state
+              """
+                 --------
+                 |      |
+                 |      
+                 |    
+                 |      
+                 |     
+                 -
+              """
+  ]
+  return stages[tries]
+end
 
+def ask_guess
+  puts 'enter ur guess'
+  input=gets.chomp
+end
 
+def hints(guess,sec_word)
   
+end
+
+def play
+  tries=0
+  sec_word=make_sec_word
+  while tries<=7
+    guess=ask_guess
+    puts display_hangman(tries)
+    tries+=1
+  end
+end
+
+play
